c3a70e052d7a5222e6a6f7d98c94ff55b224af33
johnnytal <johnnytal9@gmail.com>
2020-06-10 14:47:27 +0300

    correct

--- js/loom/light_theremin.js
+++ js/loom/light_theremin.js
@@ -15,10 +15,10 @@ var lightMain = function(game){
 	];
 	
 	config = {
-		SENSITIVITY: 75,
-		FACTOR: 3,
+		SENSITIVITY: 25,
+		FACTOR: 6,
 		SOUND: 0,
-		SCALE: 0,
+		SCALE: 0
 	};
 	
 	chromatic = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
@@ -29,13 +29,11 @@ var lightMain = function(game){
 	
 	scales = [chromatic, major, minor, pentatonic, blues];
 	
-	scaleNotes = {
+	/*scaleNotes = {
 		1:'C', 2:'C#', 3: 'D', 4: 'D#', 5: 'E', 6: 'F', 7: 'F#', 8: 'G', 9: 'G#', 10: 'A', 11: 'A#', 12: 'B',
 		13:'C', 14:'C#', 15: 'D', 16: 'D#', 17: 'E', 18: 'F', 19: 'F#', 20: 'G', 21: 'G#', 22: 'A', 23: 'A#', 24: 'B',
 		25:'C'
-	};
-	
-	playingScale = scales[config.SCALE];
+	};*/
 };
 
 lightMain.prototype = {
@@ -46,11 +44,6 @@ lightMain.prototype = {
     	debugText.anchor.set(.5, .5);
     	debugText.x = game.world.centerX;
     	debugText.y = game.world.centerY - 20;
-    	
-    	debugNote = game.add.text(0, 0, 'A#', {font: '36px', fill: 'white'});
-    	debugNote.anchor.set(.5, .5);
-    	debugNote.x = game.world.centerX;
-    	debugNote.y = game.world.centerY + 30;
 
     	lightSprite = game.add.sprite(0, 0, 'white');
     	lightSprite.anchor.set(.5, .5);
@@ -93,17 +86,13 @@ function readLight(reading){
         }
         else if (frequency_check > last_frequency){
             note++;
-            if (note > playingScale.length - 1) note = playingScale.length - 1;
+            if (note > scales[config.SCALE].length - 1) note = scales[config.SCALE].length - 1;
         }
 		
 		try{
-			sounds[config.SOUND].play(playingScale[note]);
+			sounds[config.SOUND].play(scales[config.SCALE][note]);
 		}catch(e){}	 
-		
-		try{
-			debugNote.text = scaleNotes.playingScale[note];
-		}catch(e){}	 
-		    
+  
 	    var place = Math.round(frequency_check / 60);
 	    if (place > colors.length - 1) place = colors.length - 1;
 	    game.stage.backgroundColor = '#' + colors[place];
@@ -115,13 +104,13 @@ function readLight(reading){
 }
 
 function startGUI(){
-    var gui = new dat.GUI({ width: 400 });
+    var gui = new dat.GUI({ width: 300 });
     gui.add(config, 'SENSITIVITY', 0, 500).name('Sensitivity').step(1);
     gui.add(config, 'FACTOR', 0.1, 10).name('Darkness level');
     gui.add(config, 'SOUND', { 'Vibraphone': 0, 'Glockenspiel': 1, 'Harp': 2, 'Kalimba': 3 , 'Pizzicato' : 4 }).name('Instrument');
     gui.add(config, 'SCALE', { 'Chromatic' : 0, 'Major': 1, 'Minor': 2, 'Pentatonic': 3, 'Blues': 4}).name('Scale');
     
-    if (isMobile()) gui.close();
+    //if (isMobile()) gui.close();
 }
 
 function isMobile () {
